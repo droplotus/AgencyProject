42f23e8d8ded6bbb0dccd2fe17345ce82a9bc8e0
João Sousa <50484258+droplotus@users.noreply.github.com>
2019-05-10 16:06:30 +0100

    Finished all tasks in README
    time to test even more and polish it

---
+++ AgencyProject/Menus.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "Agency.h"
+
+void menuClient(Agency &agency);
+void menuPack(Agency &agency);
+void menuInformation(Agency &agency);
+void menuHelp(Agency &agency);
+unsigned mainMenu(Agency &agency);
+void showTps(vector<Packet> tps_arr);
